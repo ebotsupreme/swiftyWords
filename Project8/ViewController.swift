b5507b0c08a5975fcf5d185e17aa2753aa71d846
Eddie Jung <ejung716@gmail.com>
2021-08-09 10:38:15 -0700

    initial commit.

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Eddie Jung on 8/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
